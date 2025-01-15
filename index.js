1afc424dd0d3dccbb6839d9ff136cd4ec6f8c87e
TerryT1 <144486954+TerryT1@users.noreply.github.com>
2025-01-15 15:34:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* === Imports === */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+
+
 
 
 /* === Firebase Setup === */
@@ -13,70 +15,109 @@ const firebaseConfig = {
     appId: "1:370766628710:web:f9396ccf4858199086eaa7"
   };
 
+
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app)
   console.log(auth)
   console.log(app.options.projectId )
 /* === UI === */
 
+
 /* == UI - Elements == */
+
 
 const viewLoggedOut = document.getElementById("logged-out-view")
 const viewLoggedIn = document.getElementById("logged-in-view")
 
+
 const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
+
 
 const emailInputEl = document.getElementById("email-input")
 const passwordInputEl = document.getElementById("password-input")
 
+
 const signInButtonEl = document.getElementById("sign-in-btn")
 const createAccountButtonEl = document.getElementById("create-account-btn")
 
+
 /* == UI - Event Listeners == */
 
+
 signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
+
 
 signInButtonEl.addEventListener("click", authSignInWithEmail)
 createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
 
+
 /* === Main Code === */
+
 
 showLoggedOutView()
 
+
 /* === Functions === */
 
+
 /* = Functions - Firebase - Authentication = */
+
 
 function authSignInWithGoogle() {
     console.log("Sign in with Google")
 }
 
+
 function authSignInWithEmail() {
     console.log("Sign in with email and password")
 }
 
+
 function authCreateAccountWithEmail() {
     console.log("Sign up with email and password")
+
+
+    const email = emailInputEl.value
+    const password= passwordInputEl.value
+
+
+
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    showLoggedInView()
+  })
+  .catch((error) => {
+    console.error(error.message)
+  });
+
 }
 
+
+
 /* == Functions - UI Functions == */
+
 
 function showLoggedOutView() {
     hideElement(viewLoggedIn)
     showElement(viewLoggedOut)
 }
 
+
 function showLoggedInView() {
     hideElement(viewLoggedOut)
     showElement(viewLoggedIn)
 }
 
+
 function showElement(element) {
     element.style.display = "flex"
 }
+
 
 function hideElement(element) {
     element.style.display = "none"
 }
 
+
 //credit: coursera
+
